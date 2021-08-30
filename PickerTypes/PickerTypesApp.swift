dd537ad1d14effeea019720d9fcedc5f7b106628
Eden <edendudoo@gmail.com>
2021-08-30 08:32:37 +0800

    Initial Commit

---
+++ PickerTypes/PickerTypesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerTypesApp.swift
+//  PickerTypes
+//
+//  Created by Eden on 2021/8/30.
+//
+
+import SwiftUI
+
+@main
+struct PickerTypesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
